b7af3ccb480e2d25236500c7f4f7c0f4a3233cc0
suvajcevic-nikola <suvajcevic1312@gmail.com>
2020-05-22 02:53:20 +0200

    Update

--- scripts/hallFame.js
+++ scripts/hallFame.js
@@ -4,8 +4,6 @@ let title = document.querySelector(".item2");
 let propform = document.querySelector(".item4");
 
 fameBtn.onclick = function() {
-    // propform.style.display = "block",
     hallFame.classList.add('show');
-    // title.style.display = "none";
     title.classList.add("hidden");
 };
